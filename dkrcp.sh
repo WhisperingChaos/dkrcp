5486f61fcf627650e12a2ac21ef78c698e9f72fd
Rich Moyse <rich@moyse.us>
2016-07-26 21:29:27 -0400

    fixes #8 - besides proper tilde expension, incorporates notion of dependent modules/plugins.

--- dkrcp.sh
+++ dkrcp.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 path_Set(){
-  local scriptFilePath
-  eval scriptFilePath\=\"$1\"
-  local scriptDir
-  scriptDir="$( dirname "$scriptFilePath")"
+  local -r scriptFilePath="$(readlink -f "$1")"
+  local -r scriptDir="$( dirname "$scriptFilePath")"
+  # include dependent utilities/libraries in path
+  if [ -d "$scriptDir/module" ]; then
+    local modDir
+    for modDir in $( ls -d "$scriptDir/module"/* ); do
+      PATH="$modDir:$PATH"
+     done
+  fi
   export PATH="$scriptDir:$PATH"
 }
 path_Set "${BASH_SOURCE[0]}"
